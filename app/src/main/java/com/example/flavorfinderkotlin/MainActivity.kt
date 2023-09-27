87f508b4ecbd68677a0c1f0d6e989b9b080c706e
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-09-27 10:13:19 +0200

    layouts

--- app/src/main/java/com/example/flavorfinderkotlin/MainActivity.kt
+++ app/src/main/java/com/example/flavorfinderkotlin/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.flavorfinderkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.flavorfinderkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
